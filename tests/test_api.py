43c4433e2e29454631164055b70e8ad261bceeb3
ravil-gasanov <12788769+ravil-gasanov@users.noreply.github.com>
2025-09-01 02:55:34 +0400

    extended the prediction api test with a mine input case

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,6 +2,7 @@ import requests
 
 
 def test_predict():
+    # input that is not a mine
     test_input = {
         "voltage": 0.283987607,
         "height": 0.181818182,
@@ -17,6 +18,25 @@ def test_predict():
     print(response.json())
 
     assert response.status_code == 200
+    assert response.json()["prediction"] == ["not a mine"]
+
+    # input that is a mine
+    test_input_is_mine = {
+        "voltage": 0.335347054,
+        "height": 0.818181818,
+        "soil": 1.0,
+    }
+
+    response = requests.post(
+        "http://localhost:8000/predict",
+        json=test_input_is_mine,
+        timeout=1,
+    )
+
+    print(response.json())
+
+    assert response.status_code == 200
+    assert response.json()["prediction"] == ["mine"]
 
 
 if __name__ == "__main__":
